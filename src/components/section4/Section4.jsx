b76d4a1b6beca2a47116724b72d6ad08a618d310
Gourab800 <gourabgorai459@gmail.com>
2026-03-11 00:46:01 +0530

    fifth commit

--- src/components/section4/Section4.jsx
+++ src/components/section4/Section4.jsx
@@ -4,22 +4,16 @@ import { BiSolidLike } from "react-icons/bi";
 function Section4(props) {
     return (
         <div>
-            <div className='bcard'> 
+            <div className='bcard'>
                 <img src={props.shake.img} alt="error" />
                 <p className='like'> <BiSolidLike /> <span>{props.shake.like}</span></p>
-
                 <div className='btm'>
-                    
-                <p className='name'>{props.shake.name}</p>
-                <div className='btn-container'>
-
-                <p>200Af</p>
-                <button>Buy Now</button>
-
+                    <p className='name'>{props.shake.name}</p>
+                    <div className='btn-container'>
+                        <p>200Af</p>
+                        <button>Buy Now</button>
+                    </div>
                 </div>
-                </div>
-
-
             </div>
         </div>
     )
